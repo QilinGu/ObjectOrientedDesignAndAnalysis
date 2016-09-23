b7ddff03d2bd140441bad4e6a410c147dd605690
Pearson Radu <pradu2@uwo.ca>
2016-09-23 14:28:09 -0400

    Created login function

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -2,12 +2,61 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 using namespace std;
 
+bool login();
+
 int main()
 {
-	cout << "just set up git on my laptop as well" << endl;
-    return 0;
+	bool entry = login();
+	cout << entry << endl;
+
+	return 0;
+}
+
+bool login()
+{
+	ifstream readFile("userdata.txt");
+	string username, password;
+	string userEnter, passEnter;
+	bool found = false;
+
+	while (!found)
+	{
+		cout << "Enter username: ";
+		cin >> username;
+
+		cout << "Enter password: ";
+		cin >> password;
+
+		if (!readFile.is_open())
+			readFile.open("userdata.txt");
+
+		string line = " ";
+		while (getline(readFile, line))
+		{
+
+			stringstream readLine(line);
+			readLine >> userEnter >> passEnter;
+
+			if (username == userEnter && password == passEnter)
+			{
+				cout << "Login Successfully!" << endl;
+				found = true;
+				break;
+			}
+		}
+
+		readFile.close();
+	}
+
+	if (found)
+		return true;
+	else
+		return false;
 }
 
